559aba013625a9510e38cd43da47455a55eb219d
qynatech <husnanur068@gmail.com>
2025-09-09 07:26:20 +0700

    connect

---
+++ connect.php
@@ -0,0 +1,15 @@
+<php?
+
+class Connection {
+
+private $hostname = "localhost";
+private $username = "root";
+private $password = "husna";
+private $database = "informasiEvent";
+
+public function connect(){
+    $connect = new mysqli($this->hostname, $this->username, $this->password, $this->database);
+}
+}
+
+?>
